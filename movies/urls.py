ee73d2ef9a05792d930652e32d6bd48be5b64948
yelim <yelim785@gmail.com>
2023-01-09 14:53:18 +0900

    Revert "feat: view 검색 기능"
    This reverts commit 3b1a1de7c6d309583c07874054f3743f0fb234b8.

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
-from movies import views
 
 app_name = 'movies'
 
 urlpatterns = [
-    # path('', views.movieSearchList, name='movie_sl'),
-    path("", views.movieSearchList, name="search"),
-    path("box/", views.movieBoxOfficeList, name="box"),
-    path("up/", views.movieUpcomingList, name="up"),
-    path("detail/<int:movie_id>", views.movieDetail.as_view(), name="detail"),
 ]
--- scripts/test_YL.py
+++ scripts/test_YL.py
@@ -1,11 +1,11 @@
 from scripts.crawler import SeleniumRequest
-from scripts.crawler.parser import idParser, infoParser
+from scripts.crawler.parser import idParser, info_parser
 
 # import os
 # from urllib.request import urlretrieve
 # from PIL import Image
 
-from movies.models import Movie, MovieReviewDummy
+from movies.models import Movie
 
 def run():
     Movie.objects.all().delete()
@@ -13,53 +13,29 @@ def run():
     request = SeleniumRequest()
 
     # years = [ 2019, 2020, 2021, 2022, 2023 ]
-    years = [ 2022, 2023 ]
-    m_ids = []
+    years = [ 2022 ]
+    datas = {}
     for year in years:
         print(f">>>>>>>>>> {year} 영화 id 가져오는 중 <<<<<<<<<<")
         url = f"https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open={year}"
     
-        m_id = request.get( url, callback = idParser )
+        data = request.get( url, callback = idParser )
         
-        m_ids += m_id
+        datas[year] = data
 
-    # 이미지 저장할 폴더 경로 
-    # img_folder_path = 'C:/final_project/final_mrs/static/imgs'
-    print("--------------------------------------")
-    print(len(m_ids))
-    print("--------------------------------------")
 
-    no = 0
     # 영화 정보 가져오기
-    for movie_id in m_ids:
-
+    for movie_id in datas[2022]:
         print(">>>>>>>>>> " + movie_id + " 정보 가져오는 중 <<<<<<<<<<")
 
         url = f"https://movie.naver.com/movie/bi/mi/point.naver?code={movie_id}"
 
-        if request.get(url, callback= infoParser) is not None:
-            movie_info, movie_review = request.get(url, callback= infoParser)
+        if request.get(url, callback= info_parser) is not None:
+            movie_info, movie_review = request.get(url, callback= info_parser)
 
-            # 이미지 저장
-            # 폴더가 없으면 새로 생성
-            # if not os.path.isdir(img_folder_path) :
-            #     os.mkdir(img_folder_path)
-
-            # img_path = './static/imgs/{}{}'.format(movie_id, '.png')
-
-            # urlretrieve(movie_info['poster'], img_path)
-
-            # image = Image.open(img_path)
-            # image = image.resize((450,600))
-            # image.save(img_path)
-
-            movie_id = int(movie_id)
+            print(movie_info)
 
             # 영화 정보 저장
             if(Movie.objects.filter(id__iexact=movie_id).count()==0) :
                 Movie(id=movie_id, **movie_info).save()
-
-
-            for review in movie_review:
-                MovieReviewDummy(no=no, movie_id=Movie.objects.get(id=movie_id), grade=review[0], review=review[1], c_date=review[2]).save()
-                no += 1
+        
