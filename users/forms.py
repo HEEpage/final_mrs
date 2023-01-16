a8c0cf24363ad8fd5d2ee8e80d175ea71dfbd148
HEEpage <page.0895059@gmail.com>
2023-01-16 19:21:33 +0900

    feat: 사용자 마이페이지 구현

--- users/forms.py
+++ users/forms.py
@@ -1,24 +1,19 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm #, PasswordChangeForm
+from django.contrib.auth import get_user_model
 from django import forms
 
-from .models import User
+from .models import User, UserMovieLog
+from movies.models import MovieGenre
 
-# 선호 장르 선택
-CHOICES_GENRE = ( ("드라마", "드라마"), ("판타지", "판타지"), ("서부", "서부"), ("공포", "공포"), 
-                ("멜로/로맨스", "멜로/로맨스"), ("모험", "모험"), ("스릴러", "스릴러"), ("느와르", "느와르"), 
-                ("컬트", "컬트"), ("다큐멘터리", "다큐멘터리"), ("코미디", "코미디"), ("가족", "가족"),
-                ("미스터리", "미스터리"), ("전쟁", "전쟁"), ("애니메이션", "애니메이션"), ("범죄", "범죄"),
-                ("뮤지컬", "뮤지컬"), ("SF", "SF"), ("액션", "액션"), ("무협", "무협"), ("에로", "에로"),
-                ("서스펜스", "서스펜스"), ("서사", "서사"), ("블랙코미디", "블랙코미디"), ("실험", "실험"), 
-                ("공연실황", "공연실황"),
-    )
 
 # 회원가입 Form
 class CreateUserForm(UserCreationForm) :
 
+    GENRE_TYPE = [ (g.type, g.type) for g in MovieGenre.objects.all() ]
+
     preference_genre = forms.MultipleChoiceField(
         widget = forms.CheckboxSelectMultiple,
-        choices = CHOICES_GENRE,
+        choices = tuple(GENRE_TYPE),
         label = "Preference Genre",
     )
 
@@ -33,3 +28,24 @@ class LoginUserForm(forms.ModelForm) :
         model = User
         fields = ["email", "password"]
 
+
+# 회원 정보 수정 Form
+class ChangeUserForm(UserChangeForm) :
+
+    GENRE_TYPE = [ (g.type, g.type) for g in MovieGenre.objects.all() ]
+
+    preference_genre = forms.MultipleChoiceField(
+        widget = forms.CheckboxSelectMultiple,
+        choices = tuple(GENRE_TYPE),
+        label = "Preference Genre",
+    )
+
+    class Meta :
+        model = get_user_model()
+        fields = ['username', 'gender', 'preference_genre']
+
+
+# 비밀번호 변경 Form
+# class PWChangeUserForm(PasswordChangeForm) :
+#     model = get_user_model()
+#     fields = ['password']
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import UserCreateView
+from . import views
 
-app_name = 'users'
+app_name = "users"
 
 urlpatterns = [
-    path("signup/", UserCreateView.as_view(), name = "signup"), # 회원가입
-    path("signin/", auth_views.LoginView.as_view(template_name="users/signin.html"), name = "signin"), # 로그인
-    path("signout/", auth_views.LogoutView.as_view(), name = "signout"), # 로그아웃
-    
+
+    path("register/", views.UserCreateView.as_view(), name = "register"), # 회원가입
+    path("update/", views.update, name = "update"), # 회원 정보 수정
+    # path("password/", password, name = "password"), # 비밀번호 변경
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name = "login"), # 로그인
+    path("logout/", auth_views.LogoutView.as_view(), name = "logout"), # 로그아웃
+
+    path("", views.UserPageView.as_view(), name = "user_page"), # 마이 페이지
+
+    path("mv-log/", views.user_mvlog_view, name = "user_mvlog"), # 영화 기록 - 목록 확인
+    path("mv-log/search/", views.user_mvlog_search_view, name = "user_mvlog_search"), # 영화 기록 - 작성할 영화 검색
+    path("mv-log/add/", views.user_mvlog_add_view, name = "user_mvlog_add"), # 영화 기록 - 등록
+    path("mv-log/write/", views.user_mvlog_write_view, name = "user_mvlog_write"), # 영화 기록 - 작성
+    path("mv-log/delete/<int:no>", views.user_mvlog_delete_view, name = "user_mvlog_delete"), # 영화 기록 - 삭제
+
+    path("wish/", views.UserWishView.as_view(), name = "user_wish"), # 위시리스트
+    path("wish/delete/<int:no>", views.user_wish_delete_view, name = "user_wish_delete"), # 위시리스트 삭제
+
 ]
