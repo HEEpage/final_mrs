f9bcbaf74dd9d5dfd3be0e469d5029708f53d1be
kyeon06 <jkyeon06@gmail.com>
2023-01-27 08:32:02 +0900

    fix: 관리자 추천 목록 json 파일 저장 코드 추가

--- movies/urls.py
+++ movies/urls.py
@@ -5,11 +5,9 @@ app_name = 'movies'
 
 urlpatterns = [
     path("", views.MovieSearchList.as_view(), name="search"),
-    path("box/", views.movieBoxOfficeList, name="box"),
-    path("up/", views.movieUpcomingList, name="up"),
     path("detail/<int:movie_id>/", views.movieDetail, name="detail"),
     path("genre/<int:genre_num>/", views.MovieGenreList.as_view(), name="all_genre"),
 
-    path("genre/recom/", views.return_recom),
+    # path("genre/recom/", views.return_recom),
 
 ]
